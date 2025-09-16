4e48be2cba360be8382b2aba30beedaa71f4b00f
Kartikeya Khosla <kartikeyakhosla07@gmail.com>
2025-09-16 11:31:24 +0530

    Sample data insertion script for 'Customer' table

---
+++ InsertStmt.sql
@@ -0,0 +1,5 @@
+INSERT INTO [dbo].[Customer] (CustomerName) VALUES ('Customer 1');
+INSERT INTO [dbo].[Customer] (CustomerName) VALUES ('Customer 2');
+INSERT INTO [dbo].[Customer] (CustomerName) VALUES ('Customer 3');
+INSERT INTO [dbo].[Customer] (CustomerName) VALUES ('Customer 4');
+INSERT INTO [dbo].[Customer] (CustomerName) VALUES ('Customer 5');
